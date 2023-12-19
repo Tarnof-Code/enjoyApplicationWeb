ab8d0638232f3da42ec81deaf895cc8a1cbc5fa5
Tarnof <tarnof@hotmail.com>
2023-12-19 17:37:46 +0100

    Cartes infos et photo dans Profil admin / Modification couleur navbar

--- src/Pages/Profil/Profil.jsx
+++ src/Pages/Profil/Profil.jsx
@@ -1,9 +1,12 @@
 import styles from "./Profil.module.scss";
 import React, { useEffect, useState } from "react";
+import { Container, Row, Col, Card, CardBody, Button } from "reactstrap";
 import formaterDate from "../../helpers/formaterDate";
 import { accountService } from "../../services/account.service";
 import { utilisateurService } from "../../services/utilisateur.service";
 import { Navigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPencilAlt } from "@fortawesome/free-solid-svg-icons";
 
 function Profil() {
   const [loading, setLoading] = useState(true);
@@ -40,35 +43,67 @@ function Profil() {
   if (!accountService.isLogged()) return <Navigate to="/" />;
 
   return (
-    <div className={styles.container}>
+    <Container fluid className={styles.container}>
       {loading ? (
         <p>Chargement en cours...</p>
       ) : (
-        <div className={styles.profilBox}>
-          <div className={styles.title_box}>
-            <h1>Mon profil</h1>
-          </div>
-          <div className={styles.content_box}>
-            <div className={styles.infos_box}>
-              {propertyMappings.map((mapping) => (
-                <div key={mapping.display} className={styles.infos_row}>
-                  <div className={styles.infos_content}>
-                    <div className={styles.label}>{mapping.display}</div>
-                    <div className={styles.value}>
-                      {mapping.isDate
-                        ? formaterDate(utilisateur[mapping.property]) // Appeler le formateur de date
-                        : utilisateur[mapping.property]}
+        <Container>
+          <Row className={styles.title}>
+            <Col>
+              <h1>Mon profil</h1>
+            </Col>
+          </Row>
+          <Row>
+            <Col md={2}>
+              <Card className={`${styles.card} mt-4`}>
+                <CardBody className={styles.photo_card}>
+                  <img
+                    src="https://bootdey.com/img/Content/avatar/avatar1.png"
+                    alt=""
+                    className={styles.profile_img}
+                  ></img>
+                  <Button className={styles.button} target="__blank" href="">
+                    Modifier
+                  </Button>
+                </CardBody>
+              </Card>
+            </Col>
+            <Col md={5}>
+              <Card className={`${styles.card} mt-4`}>
+                <CardBody>
+                  {propertyMappings.map((mapping) => (
+                    <div key={mapping.display}>
+                      <Row>
+                        <Col sm={5}>
+                          <h6 className="mb-0">{mapping.display}</h6>
+                        </Col>
+                        <Col sm={6} className="text-secondary">
+                          {mapping.isDate
+                            ? formaterDate(utilisateur[mapping.property])
+                            : utilisateur[mapping.property]}
+                        </Col>
+                      </Row>
+                      <hr className={styles.separation_line} />
                     </div>
-                  </div>
-                  <div className={styles.infos_edit}></div>
-                </div>
-              ))}
-            </div>
-            <div className={styles.photo_box}></div>
-          </div>
-        </div>
+                  ))}
+                  <Row>
+                    <Col sm={12} className="text-center">
+                      <Button
+                        className={styles.button}
+                        target="__blank"
+                        href="https://www.bootdey.com/snippets/view/profile-edit-data-and-skills"
+                      >
+                        Modifier
+                      </Button>
+                    </Col>
+                  </Row>
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
       )}
-    </div>
+    </Container>
   );
 }
 
