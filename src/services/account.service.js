aadd56e886b67616362cf02f0783fb2a191bcb54
Tarnof <tarnof@hotmail.com>
2025-09-23 16:09:29 +0200

    refactor: improve account and user services readability and consistency

--- src/services/account.service.js
+++ src/services/account.service.js
@@ -1,103 +1,101 @@
-import Axios from './caller.service'
-import { jwtDecode } from 'jwt-js-decode';
-import Cookies from 'universal-cookie';
+import Axios from "./caller.service";
+import { jwtDecode } from "jwt-js-decode";
 import store from "../redux/store";
 import { clearUser } from "../redux/auth/authSlice";
 import CryptoJS from "crypto-js";
-import { secretKey } from "../../config.local.js"
-
-const cookies = new Cookies();
+import { secretKey } from "../../config.local.js";
 
 /**
  * Connexion vers l'API
- * @param {object} credentials 
+ * @param {object} credentials
  * @returns {Promise}
  */
 const login = async (credentials) => {
-    try {
-        const response = await Axios.post('/auth/connexion', credentials, {
-            withCredentials: true,
-            headers: {
-                'X-Skip-Token-Refresh': true, // En tête personnalisée pour éviter l'interceptor
-            },
-        });
-        console.log(response)
-        return response;
-    } catch (error) {
-        throw error;
-    }
+  const response = await Axios.post("/auth/connexion", credentials, {
+    withCredentials: true,
+    headers: {
+      "X-Skip-Token-Refresh": true, // En tête personnalisée pour éviter l'interceptor
+    },
+  });
+  console.log(response);
+  return response;
 };
 
 let addUser = async (userInfos) => {
-    try {
-        const response = await Axios.post('/auth/inscription', userInfos, {
-            withCredentials: true,
-            headers: {
-                'X-Skip-Token-Refresh': true, // En tête personnalisée pour éviter l'interceptor
-            },
-        });
-        return response;
-    } catch (error) {
-        throw error;
-    }
-}
+  const response = await Axios.post("/auth/inscription", userInfos, {
+    withCredentials: true,
+    headers: {
+      "X-Skip-Token-Refresh": true, // En tête personnalisée pour éviter l'interceptor
+    },
+  });
+  return response;
+};
 
 /**
  * Sauvegarde du token dans le localStorage
- * @param {string} access_token 
+ * @param {string} access_token
  */
 let saveAccessToken = (access_token) => {
-    let crypted_access_token = CryptoJS.AES.encrypt(access_token, secretKey).toString();
-    localStorage.setItem('access_token', crypted_access_token)
-    console.log(access_token)
-    console.log(crypted_access_token)
-}
-
+  let crypted_access_token = CryptoJS.AES.encrypt(
+    access_token,
+    secretKey
+  ).toString();
+  localStorage.setItem("access_token", crypted_access_token);
+  console.log(access_token);
+  console.log(crypted_access_token);
+};
 
 /**
  * Suppression du token du localStorage
  */
 let logout = () => {
-    localStorage.removeItem('access_token')
-    store.dispatch(clearUser());
-    window.location.href = "/";
-}
+  localStorage.removeItem("access_token");
+  store.dispatch(clearUser());
+  window.location.href = "/";
+};
 
 /**
  * Etat de la présence d'un token en localStorage
  * @returns {boolean}
  */
 let isLogged = () => {
-    let access_token = localStorage.getItem('access_token')
-    return !!access_token
-}
+  let access_token = localStorage.getItem("access_token");
+  return !!access_token;
+};
 
 /**
  * Récupération brut du token en localStorage
  * @returns {string}
  */
 let getToken = () => {
-    let crypted_access_token = localStorage.getItem('access_token');
-    let bytes = CryptoJS.AES.decrypt(crypted_access_token, secretKey);
-    let originalText = bytes.toString(CryptoJS.enc.Utf8);
-    return originalText;
-}
+  let crypted_access_token = localStorage.getItem("access_token");
+  let bytes = CryptoJS.AES.decrypt(crypted_access_token, secretKey);
+  let originalText = bytes.toString(CryptoJS.enc.Utf8);
+  return originalText;
+};
 
 /**
  * Récupération du payload du tkoen
  * @returns {object}
  */
 let getTokenInfo = () => {
-    return jwtDecode(getToken())
-}
+  return jwtDecode(getToken());
+};
 
 let refreshAccessToken = async () => {
-    logout();
-    const response = await Axios.post('/auth/refresh-token')
-    return response
-}
+  logout();
+  const response = await Axios.post("/auth/refresh-token");
+  return response;
+};
 
 // Déclaration des serivces pour import
 export const accountService = {
-    login, addUser, saveAccessToken, logout, isLogged, getToken, getTokenInfo, refreshAccessToken
-}
+  login,
+  addUser,
+  saveAccessToken,
+  logout,
+  isLogged,
+  getToken,
+  getTokenInfo,
+  refreshAccessToken,
+};
--- src/services/utilisateur.service.js
+++ src/services/utilisateur.service.js
@@ -1,118 +1,121 @@
-import Axios from './caller.service'
-import { accountService } from './account.service'
-import store from "../redux/store"
-import { setUser } from '../redux/auth/authSlice'
-import { useSelector } from "react-redux";
-
+import Axios from "./caller.service";
+import { accountService } from "./account.service";
+import store from "../redux/store";
+import { setUser } from "../redux/auth/authSlice";
 
 let getAllUsers = async () => {
-    try {
-        let response = await Axios.get('/utilisateurs/liste');
-        response.data.forEach(user => {
-            if (user.dateExpirationCompte) {
-                user.dateExpirationCompte = user.dateExpirationCompte * 1000;
-            }
-            user.nom = user.nom.toUpperCase();
-        });
-        response.data.sort((a, b) => {
-            return a.nom.toLocaleLowerCase().localeCompare(b.nom.toLocaleLowerCase());
-        });
-        return response;
-    } catch {
-        throw new Error('Impossible de récupérer la liste des utilisateurs')
-    }
-}
+  try {
+    let response = await Axios.get("/utilisateurs/liste");
+    response.data.forEach((user) => {
+      if (user.dateExpirationCompte) {
+        user.dateExpirationCompte = user.dateExpirationCompte * 1000;
+      }
+      user.nom = user.nom.toUpperCase();
+    });
+    response.data.sort((a, b) => {
+      return a.nom.toLocaleLowerCase().localeCompare(b.nom.toLocaleLowerCase());
+    });
+    return response;
+  } catch {
+    throw new Error("Impossible de récupérer la liste des utilisateurs");
+  }
+};
 
 let getUser = async () => {
-    try {
-        let token_infos = accountService.getTokenInfo();
-        const tokenId = token_infos.payload.sub;
-        const response = await Axios.get(`/utilisateurs/profil?tokenId=${tokenId}`, {
-            withCredentials: true
+  try {
+    let token_infos = accountService.getTokenInfo();
+    const tokenId = token_infos.payload.sub;
+    const response = await Axios.get(
+      `/utilisateurs/profil?tokenId=${tokenId}`,
+      {
+        withCredentials: true,
+      }
+    );
+    if (response) {
+      response.data.dateExpirationCompte =
+        response.data.dateExpirationCompte * 1000;
+      store.dispatch(
+        setUser({
+          role: response.data.role,
+          prenom: response.data.prenom,
+          genre: response.data.genre,
         })
-        if (response) {
-            response.data.dateExpirationCompte = response.data.dateExpirationCompte * 1000;
-            store.dispatch(setUser({ role: response.data.role, prenom: response.data.prenom, genre: response.data.genre }));
-            return response
-        }
-
-    } catch {
-        throw new Error("access_token est manquant ou erreur de réseau")
+      );
+      return response;
     }
-}
+  } catch {
+    throw new Error("access_token est manquant ou erreur de réseau");
+  }
+};
 
 let updateUser = async (utilisateur) => {
-    try {
-        // let token_infos = accountService.getTokenInfo();
-        // const tokenId = token_infos.payload.sub;
-        // utilisateur.tokenId = tokenId;
-        const response = await Axios.put('/utilisateurs/modifierInfos', utilisateur, {
-            withCredentials: true,
-            headers: {
-                'X-Skip-Token-Refresh': true, // En tête personnalisée pour éviter l'interceptor
-            },
-        });
-        return response;
-    } catch (error) {
-        //  console.error("Une erreur s'est produite lors de la mise à jour de l'utilisateur :", error.response.data.message);
-        throw error;
-    }
+  // let token_infos = accountService.getTokenInfo();
+  // const tokenId = token_infos.payload.sub;
+  // utilisateur.tokenId = tokenId;
+  const response = await Axios.put("/utilisateurs/modifierInfos", utilisateur, {
+    withCredentials: true,
+    headers: {
+      "X-Skip-Token-Refresh": true, // En tête personnalisée pour éviter l'interceptor
+    },
+  });
+  return response;
 };
-
 
 let getRoleByGenre = (role, genre) => {
-    switch (genre) {
-        case "Feminin":
-            switch (role) {
-                case "ADMIN":
-                    role = "ADMINISTRATRICE";
-                    break;
-                case "DIRECTION":
-                    role = "DIRECTRICE";
-                    break;
-                case "ADJ_DIRECTION":
-                    role = "ADJOINTE";
-                    break;
-                case "ANIM":
-                    role = "ANIMATRICE";
-                    break;
-                case "ANIM_AS":
-                    role = "ANIMATRICE_AS";
-                    break;
-                default:
-                    break;
-            }
-            break;
-
-        case "Masculin":
-            switch (role) {
-                case "ADMIN":
-                    role = "ADMINISTRATEUR";
-                    break;
-                case "DIRECTION":
-                    role = "DIRECTEUR";
-                    break;
-                case "ADJ_DIRECTION":
-                    role = "ADJOINT";
-                    break;
-                case "ANIM":
-                    role = "ANIMATEUR";
-                    break;
-                case "ANIM_AS":
-                    role = "ANIMATEUR_AS";
-                    break;
-                default:
-                    break;
-            }
-            break;
-
+  switch (genre) {
+    case "Feminin":
+      switch (role) {
+        case "ADMIN":
+          role = "ADMINISTRATRICE";
+          break;
+        case "DIRECTION":
+          role = "DIRECTRICE";
+          break;
+        case "ADJ_DIRECTION":
+          role = "ADJOINTE";
+          break;
+        case "ANIM":
+          role = "ANIMATRICE";
+          break;
+        case "ANIM_AS":
+          role = "ANIMATRICE_AS";
+          break;
         default:
-            break;
-    }
-    return role;
+          break;
+      }
+      break;
+
+    case "Masculin":
+      switch (role) {
+        case "ADMIN":
+          role = "ADMINISTRATEUR";
+          break;
+        case "DIRECTION":
+          role = "DIRECTEUR";
+          break;
+        case "ADJ_DIRECTION":
+          role = "ADJOINT";
+          break;
+        case "ANIM":
+          role = "ANIMATEUR";
+          break;
+        case "ANIM_AS":
+          role = "ANIMATEUR_AS";
+          break;
+        default:
+          break;
+      }
+      break;
+
+    default:
+      break;
+  }
+  return role;
 };
 
-
 export const utilisateurService = {
-    getAllUsers, getUser, getRoleByGenre, updateUser
-}
+  getAllUsers,
+  getUser,
+  getRoleByGenre,
+  updateUser,
+};
